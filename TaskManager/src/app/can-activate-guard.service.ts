42b01118b960c802d22830d9d6444227fd9cf1ef
sazee <saikumar.bellide@gmail.com>
2024-10-07 19:40:57 +0530

    CanActivate Guard

--- TaskManager/src/app/can-activate-guard.service.ts
+++ TaskManager/src/app/can-activate-guard.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { LoginService } from './login.service';
 import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CanActivateGuardService implements CanActivate {
 
-  constructor(private loginService:LoginService,private router:Router) { }
+  constructor(private loginService:LoginService,private router:Router,private jwtHelperService:JwtHelperService) { }
   canActivate(route:ActivatedRouteSnapshot):boolean {
+    var token = sessionStorage.getItem("currentUSer") ? JSON.parse(sessionStorage.getItem("currentUSer") as string).token:null;
     console.log(this.router.url);
-    if(this.loginService.isAuthenticated())
+    if(this.loginService.isAuthenticated() || this.jwtHelperService.decodeToken(token).role == route.data["expectedRole"])
       {
         return true;
       }
