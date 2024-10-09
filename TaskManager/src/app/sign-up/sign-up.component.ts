3aff21f05980699afa3c60ef3eb7192c84d59d71
sazee <saikumar.bellide@gmail.com>
2024-10-09 18:22:03 +0530

    Dynamic Dropdown

--- TaskManager/src/app/sign-up/sign-up.component.ts
+++ TaskManager/src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { CountriesService } from '../countries.service';
+import { Country } from '../country';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,25 +12,29 @@ export class SignUpComponent implements OnInit {
 
   signUpForm:FormGroup;
   genders=["Male","Female"];
-
-  constructor(){
+  countries:Country[] =[];
+  constructor(private countriesService:CountriesService){
     this.signUpForm = new FormGroup({
       firstName:new FormControl(null),
       lastName:new FormControl(null),
       email:new FormControl(null),
       mobile:new FormControl(null),
       dateOfBirt:new FormControl(null),
-      gender:new FormControl(null)
+      gender:new FormControl(null),
+      countryID:new FormControl(null)
      });
   }
   ngOnInit(): void {
+
+    this.countries = this.countriesService.getCountries();
     this.signUpForm = new FormGroup({
      firstName:new FormControl(null),
      lastName:new FormControl(null),
      email:new FormControl(null),
      mobile:new FormControl(null),
      dateOfBirt:new FormControl(null),
-     gender:new FormControl(null)
+     gender:new FormControl(null),
+     countryID:new FormControl(null)
     });
   }
 }
